f42be289c4561a89c3cbef937240ec37f4916636
Aldrin <aldrin09176@yahoo.com>
2023-08-04 03:18:06 +0800

    selenium 4.11 update

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/features/",
+@CucumberOptions(features="src/test/resources/features/Login.feature",
 				 glue={"stepdefinitions","hooks"},
 				 publish=true,
 				 plugin={"pretty","html:target/CucumberReports/CucumberReport.html",
